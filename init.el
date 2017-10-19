01a42df6a423e9f0f94261b37ee6d78f999ef6f9
thu <wxxaca@gmail.com>
2017-10-19 15:09:54 -0400

    update init.el

--- init.el
+++ init.el
@@ -8,13 +8,32 @@
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
-;;;;;;;;;;;;;;;;;;;;; company ;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;; packages ;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'company)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
 
-;; Enable company-mode
-(global-company-mode t)
+(require 'cl)
 
+;; add whatever packages you want
+(defvar wxx/packages '(company
+		       monokai-theme
+		       hungry-delete
+		       counsel
+		       swiper
+		       ) "Default packages")
+(defun wxx/packages-installed-p ()
+  (loop for pkg in wxx/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+(unless (wxx/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg wxx/packages)
+    (when (not (package-installed-p pkg))
+        (package-install pkg))))
 
 ;;;;;;;;;;;;;;;;;;;;; org-mode ;;;;;;;;;;;;;;;;;;;;;;
 
@@ -35,6 +54,34 @@
 
 ;; Shortcuts for open recent files
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;;;;;;;;;;;;;;;;;;;;; company ;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'company)
+
+;; Enable company-mode
+(global-company-mode t)
+
+;;;;;;;;;;;;;;;;;;;;; Theme ;;;;;;;;;;;;;;;;;;;;;;;;;
+(load-theme 'monokai t)
+
+;;;;;;;;;;;;;;;;;; hungry-delete ;;;;;;;;;;;;;;;;;;;;
+
+(require  'hungry-delete)
+(global-hungry-delete-mode t)
+
+;;;;;;;;;;;;;;;;;;;;; swiper ;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ivy-mode t)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+
+;; Shortcuts for swiper
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 
 ;;;;;;;;;;;;;;;;;;;;; Common ;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -59,3 +106,6 @@
 
 ;; Highlight current line
 (global-hl-line-mode t)
+
+;; Enable paren-mode
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
