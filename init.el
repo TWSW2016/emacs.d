2b7903943d4e3347f122f677c5f2ada103307d70
thu <wxxaca@gmail.com>
2017-10-19 14:16:08 -0400

    update init.el

--- init.el
+++ init.el
@@ -1,13 +1,42 @@
 (package-initialize)
 
+;;;;;;;;;;;;;;;;;;;;; Basic ;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Key-binding to ~/.emacs.d/init.el
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
+;;;;;;;;;;;;;;;;;;;;; company ;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'company)
+
 ;; Enable company-mode
-(setq global-company-mode t)
+(global-company-mode t)
+
+
+;;;;;;;;;;;;;;;;;;;;; org-mode ;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'org)
+
+;; Enable fontify natively
+(setq org-src-fontify-natively t)
+
+;;;;;;;;;;;;;;;;;;;;; recentf ;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'recentf)
+
+;; Enable recentf-mode
+(recentf-mode t)
+
+;; Set maximum size of items of recentf menu
+(setq recentf-max-menu-items 25)
+
+;; Shortcuts for open recent files
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;;;;;;;;;;;;;;;;;;;;; Common ;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Disable tool-bar
 (setq tool-bar-mode nil)
@@ -21,6 +50,12 @@
 ;; Disable splash page
 (setq inhibit-splash-screen t)
 
-;; Enable recentf-mode
-(setq recentf-mode t)
+;; Disable backup files
+(setq make-backup-files nil)
 
+;; Maximize screen
+(setq initial-frame-alist 
+      (quote ((fullscreen . maximized))))
+
+;; Highlight current line
+(global-hl-line-mode t)
